71cb27848b3e4bf94d81a4e5497c854e8c6ebec9
jasperm2 <mustasiltajasper@gmail.com>
2026-02-10 22:26:02 +0200

    viikko1 muokkaukset

--- src/index.js
+++ src/index.js
@@ -1,13 +1,50 @@
-// index.js
-import http from 'http';
+import express from 'express';
 const hostname = '127.0.0.1';
+const app = express();
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Welcome to my REST API!');
+// Dummy mock data (nollautuu aina, kun sovelluksen käynnistää uudelleen)
+const items = [
+  {id: 1, name: 'Omena'},
+  {id: 2, name: 'Appelsiini'},
+  {id: 3, name: 'Banaaneja'},
+];
+
+// parsitaan json data pyynnöstä ja lisätään request-objektiin
+app.use(express.json());
+
+// API root
+app.get('/', (req, res) => {
+  res.send('This is dummy items API!');
 });
 
-server.listen(port, hostname, () => {
+// Get all items
+app.get('/items', (req, res) => {
+  res.json(items);
+});
+
+// Get item based on id
+app.get('/items/:id', (req, res) => {
+  console.log('getting item id:', req.params.id);
+  const itemFound = items.find(item => item.id == req.params.id);
+  if (itemFound) {
+    res.json(itemFound);
+  } else {
+    res.status(404).json({message: 'item not found'});
+  }
+});
+
+// TODO: add PUT route for items
+// TODO: add DELETE route for items
+
+// Add new item
+app.post('/items', (req, res) => {
+  //console.log('add item request body', req.body);
+  // TODO: lisää id listaan lisättävälle objektille
+  items.push(req.body);
+  res.status(201).json({message: 'new item added'});
+});
+
+app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
